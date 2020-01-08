9693d0e10a068d817737c4ea330b0895e2e5ab60
Your Name <shubhamvrmcool@gmail.com>
2020-01-08 21:37:03 +0530

    Add raw files

---
+++ omni/src/test/java/com/cash/omni/OmniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cash.omni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
